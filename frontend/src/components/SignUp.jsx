af20673dff76afcbf546877bb62cf599b3263c9c
HP <chandan.workholics@gmail.com>
2025-05-22 11:37:20 +0530

    updated

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -205,7 +205,7 @@ const SignUp = () => {
                   ) : (
                     "Sign Up"
                   )}</button>
-                  <h6 className="text-center text-445B64 mb-3">Don't have an account? <Link to='/' className='text-00C7BE text-decoration-none'> Sign in</Link></h6>
+                  <h6 className="text-center text-445B64 mb-3">Already have an account? <Link to='/' className='text-00C7BE text-decoration-none'> Sign in</Link></h6>
                 </div>
               </div>
             </div>
